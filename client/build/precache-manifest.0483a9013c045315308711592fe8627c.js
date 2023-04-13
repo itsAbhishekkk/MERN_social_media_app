1b23948d28a7663752593267426cba38a3bfd4af
oyeAbhisheksingh <100716177+oyeAbhisheksingh@users.noreply.github.com>
2023-04-13 18:55:01 +0530

    first commit

---
+++ client/build/precache-manifest.0483a9013c045315308711592fe8627c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63b6a5a13f386d1bd7c8",
+    "url": "/static/css/main.f30d4ba2.chunk.css"
+  },
+  {
+    "revision": "63b6a5a13f386d1bd7c8",
+    "url": "/static/js/main.63b6a5a1.chunk.js"
+  },
+  {
+    "revision": "de5be217f2b9e0ed28be",
+    "url": "/static/js/1.de5be217.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "943ec8ff77d2ddcc41812aba25f2e2b1",
+    "url": "/index.html"
+  }
+];
